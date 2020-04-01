f270ac58d9d6491141fd7a9152689b258975aa3b
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-04-02 00:11:31 +0200

    init

--- csv_file_validator/__main__.py
+++ csv_file_validator/__main__.py
@@ -1,23 +1,31 @@
+import logging
+import csv
 from csv_file_validator import validations
 
+# logger = logging.Logger(name=__name__)
+
 config = {
-    'file_name_file_mask': 'test*',
-    'file_extension': 'csv',
-    'file_size_range': [0, 1],
-    'file_row_count_range': [0, 50],
-    'file_value_separator': ',',
-    'file_has_header': True,
-    'file_header_column_names': ['col1', 'col2', 'col3'],
+    'file_metadata': {
+        'file_value_separator': ',',
+        'file_has_header': True,
+    },
+    'file_validation_rules': {
+        'file_name_file_mask': 'SalesJ',
+        'file_extension': 'csv',
+        'file_size_range': [0, 1],
+        'file_row_count_range': [0, 1000],
+        'file_header_column_names': ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'Name', 'City', 'State', 'Country', 'Account_Created', 'Last_Login', 'Latitude', 'Longitude']
+    },
     'column_validation_rules': {
-        'col1': {
+        'Transaction_date': {
             'allow_numeric_value_range': [0, 100],
             'allow_data_type': 'str',
         },
-        'col2': {
+        'Country': {
             'allow_fixed_value_list': ['yyy', 'www'],
             'allow_regex': '$#%@%^@',
         },
-        'col3': {
+        'Price': {
             'allow_substring': 'xxzerw',
             'allow_fixed_value': 'xxx',
             'allow_data_type': 'str'
@@ -32,12 +40,10 @@ class Validate:
         attribute_func_map = {
             "file_name_file_mask": validations.check_filemask,
             "file_extension": validations.check_file_extension,
-            # "file_size_range": validations.check_file_size_range,
-            # "file_row_count_range": validations.check_file_row_count_range,
-            # "file_value_separator": validations.check_file_value_separator,
-            # "file_has_header": validations.check_file_has_header,
-            # "file_header_column_names": validations.check_file_header_column_names,
-            # "file_column_datatypes": validations.check_file_column_datatypes,
+            "file_size_range": validations.check_file_size_range,
+            "file_row_count_range": validations.check_file_row_count_range,
+            "file_header_column_names": validations.check_file_header_column_names,
+            "allow_data_type": validations.check_column_allow_data_type,
             # "column_allow_numeric_value_range": validations.check_column_allow_numeric_value_range,
             # "column_allow_fixed_value_list": validations.check_column_allow_fixed_value_list,
             # "column_allow_regex": validations.check_column_allow_regex,
@@ -51,28 +57,31 @@ class Validate:
         else:
             return None
 
-    @staticmethod
-    def file_read_generator(file):
-        for row in open(file, encoding="utf8", mode='r'):
-            yield row
+    def _get_file_header(self):
+        self.file_header = open(file, mode='r', encoding='utf8').readline().split(self._get_file_metadata_value('file_value_separator'))
+
+    def file_read_generator(self, file):
+        self._get_file_header()
+
+        for row in open(file, mode='r', encoding='utf8'):
+            yield dict(zip(self.file_header,row.split(self._get_file_metadata_value('file_value_separator'))))
 
     def __init__(self, config):
         self.config = config
 
+    # TODO
     def _validate_config_file(self):
-        pass
+        if not isinstance(self.config, dict):
+            raise Exception
 
     def _get_file_level_validations(self):
-        return {k: v for k, v in self.config.items() if k.startswith('file')}
+        return {k: v for k, v in self.config.get('file_validation_rules').items()}
 
-    def _get_column_level_validations(self):
+    def _get_column_level_validations(self, column):
         return {k: v for k, v in self.config.get('column_validation_rules').items()}
 
-    def _get_all_validations(self):
-        return {**self._get_file_level_validations(), **self._get_column_level_validations()}
-
-    def _get_validation(self, validation_rule):
-        return {k: v for k, v in self._get_all_validations().items() if k == validation_rule}
+    def _get_file_metadata_value(self, metavalue):
+        return [v for k, v in self.config.get('file_metadata').items() if k == metavalue][0]
 
     def validate_file(self, file):
         file_level_validations = self._get_file_level_validations()
@@ -80,9 +89,10 @@ class Validate:
             self.function_caller(validation, **{'file': file, 'arg1': value})
 
     def validate_line(self, line):
-        column_level_validations = self._get_column_level_validations()
-        for validation, value in column_level_validations.items():
-            self.function_caller(validation, **{'line': line, 'arg1': value})
+        column_level_validations = self._get_column_level_validations(column='x')
+        for column, validations in column_level_validations.items():
+            for validation, value in validations.items():
+                self.function_caller(validation, **{'column': column, 'arg1': value})
 
 
 files_list = ["SalesJan2009.csv"]
@@ -92,9 +102,3 @@ for file in files_list:
     vobj.validate_file(file)
     for line in vobj.file_read_generator(file):
         vobj.validate_line(line)
-
-# print(Validate._get_file_level_validations(vobj))
-# print(Validate._get_column_level_validations(vobj))
-# print(Validate._get_all_validations(vobj))
-# print(Validate.function_caller('file_extension', **{'arg1': "csv"}))
-# print(Validate.function_caller('file_mask', **{'arg1': "xqw"}))
--- csv_file_validator/validations.py
+++ csv_file_validator/validations.py
@@ -1,39 +1,9 @@
 import os, re
+import logging
 
-def file_name_filemask(file_name):
-    pass
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-
-def file_extension(file_extension):
-    pass
-
-
-def file_size_range(file_size):
-    pass
-
-
-def file_value_separator():
-    pass
-
-
-def row_count_range(df):
-    pass
-
-
-def file_has_header():
-    pass
-
-
-def file_header_column_names():
-    pass
-
-
-def file_column_datatypes():
-    pass
-
-
-def allow_numeric_value_range():
-    pass
 
 
 def allow_fixed_value():
@@ -52,35 +22,53 @@ def allow_substring():
     pass
 
 
-def check_file_extension(**kwargs) -> bool:
-    """
-    checks the file extension matches value in validation_schema
-    """
-    # rfind() returns -1 if it doesn't find the character it's looking for
-    # if self.file.rfind('.') >= 0:
-    #     dot_index = self.file.rfind('.')
-    #     result = self.file[dot_index + 1:] == file_extension
-    #     return result
-    # else:
-    #     result = file_extension == ''
-    #     return result
-    return kwargs.items()
+def check_file_extension(**kwargs):
+    if os.path.splitext(kwargs.get('file'))[1][1:] == kwargs.get('arg1'):
+        logger.info('check_file_extension passed')
+    else:
+        logger.warning('check_file_extension failed')
 
-def check_filemask(**kwargs) -> bool:
-    """
-    checks the filename matches the regex pattern stipulated in the
-    validation_schema
-    """
 
-    # file = os.path.split(self.file)[-1]
-    # dot_index = file.rfind('.')
-    # filename = file[:dot_index]
-    # result = re.match(filemask, filename)
-    #
-    # # Check if regexp didn't match anything
-    # if result is None:
-    #     return False
-    # else:
-    #     return True
-    print(kwargs)
-    return kwargs.items()
+def check_filemask(**kwargs):
+    file = os.path.split(kwargs.get('file'))[-1]
+    dot_index = file.rfind('.')
+    filename = file[:dot_index]
+
+    result = re.match(kwargs.get('arg1'), filename)
+
+    # Check if regexp didn't match anything
+    if result:
+        logger.info('check_filemask passed')
+    else:
+        logger.warning('check_filemask failed')
+
+
+def check_file_size_range(**kwargs):
+    fsize = os.path.getsize(kwargs.get('file')) / 1024 / 1024
+
+    if fsize >= kwargs.get('arg1')[0] and fsize <= kwargs.get('arg1')[1]:
+        logger.info('check_file_size_range passed')
+    else:
+        logger.warning('check_file_size_range failed')
+
+
+def check_file_row_count_range(**kwargs):
+    #TODO use the generator in __main__
+    num_lines = sum(1 for line in open(kwargs.get('file')))
+
+    if num_lines >= kwargs.get('arg1')[0] and num_lines <= kwargs.get('arg1')[1]:
+        logger.info('check_file_row_count_range passed')
+    else:
+        logger.warning('check_file_row_count_range failed')
+
+def check_file_header_column_names(**kwargs):
+    #TODO use the generator in __main__
+    first_line = [line for line in open(kwargs.get('file'))][0].split(',')
+
+    if first_line == kwargs.get('arg1'):
+        logger.info('check_file_header_column_names passed')
+    else:
+        logger.warning('check_file_header_column_names failed')
+
+def check_column_allow_data_type(**kwargs):
+    if isinstance(kwargs.get(''))
